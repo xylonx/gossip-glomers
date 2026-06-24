b0669901082c992a260bcb3680faf455392404e3
xylonx <xylonx@xylonx.com>
2026-06-24 11:57:54 +0800

    feat: pass lab2 unique-ids

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: maelstrom-echo
+.PHONY: maelstrom-echo maelstrom-uniq-id
 
 MAELSTROM=${HOME}/repo/maelstrom/maelstrom/maelstrom
 
-./target/debug/echo: ./src/bin/echo.rs
-	cargo build
+echo-bin: ./src/bin/echo.rs
+	cargo build --bin echo
 
-maelstrom-echo: ./target/debug/echo
+maelstrom-echo: echo-bin
 	$(MAELSTROM) test -w echo --bin ./target/debug/echo --time-limit 5
+
+uniq-ids-bin: ./src/bin/uniq-ids.rs
+	cargo build --bin uniq-ids
+
+maelstrom-uniq-id: uniq-ids-bin
+	$(MAELSTROM) test -w unique-ids --bin ./target/debug/uniq-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
---
+++ src/bin/uniq-ids.rs
@@ -0,0 +1,85 @@
+use std::{
+    cell::OnceCell,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+use gossip_glomers::maelstrom::{
+    Serve, error::Result, handler::Handler, message::MessageId, runtime::Runtime,
+};
+use serde::{Deserialize, Serialize};
+use tokio::io::{BufReader, BufWriter};
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum UniqueIdPayload {
+    Generate,
+    GenerateOk { id: u64 },
+}
+
+/// The implementation splits u64 value space into partitions where each node own a partition.
+///
+/// [ n1 ][ n2 ][ ... ][ n_{n} ]
+/// 0                          U64::MAX
+///
+/// So it allows availability even network partitions as there is no communication among nodes.
+/// But it is NOT Fault-Tolerant as the counter will **reset** after restart the node.
+#[derive(Debug)]
+struct UniqueIdHandler {
+    msg_id: AtomicU64,
+
+    counter: OnceCell<AtomicU64>,
+}
+
+impl UniqueIdHandler {
+    pub fn new() -> Self {
+        Self {
+            msg_id: AtomicU64::new(0),
+            counter: OnceCell::new(),
+        }
+    }
+}
+
+impl Handler for UniqueIdHandler {
+    type T = UniqueIdPayload;
+
+    async fn handle(
+        &self,
+        runtime: &Runtime<Self::T>,
+        _: Option<MessageId>,
+        _: Self::T,
+    ) -> Result<Option<(Option<MessageId>, Self::T)>> {
+        let next_id = self.counter.get_or_init(|| {
+            let offset = runtime
+                .node
+                .cluster
+                .iter()
+                .position(|r| r == &runtime.node.id)
+                .unwrap(); // node must be in cluster. It is SAFE to unwrap here
+
+            let cluster_size = runtime.node.cluster.len();
+
+            let partition_size = u64::MAX / (cluster_size as u64);
+
+            AtomicU64::new(partition_size * (offset as u64))
+        });
+
+        let id = next_id.fetch_add(1, Ordering::AcqRel);
+        let msg_id = MessageId::new(self.msg_id.fetch_add(1, Ordering::AcqRel));
+
+        Ok(Some((Some(msg_id), UniqueIdPayload::GenerateOk { id })))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::Layer::new().with_writer(std::io::stdout).pretty())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let serve = Serve::new(UniqueIdHandler::new());
+    let reader = BufReader::new(tokio::io::stdin());
+    let writer = BufWriter::new(tokio::io::stdout());
+    serve.serve(reader, writer).await
+}
